3202ee7a56fb11b84a7c0274f4baf7370f162481
rschuessler <roderick.schuessler8@googlemail.com>
2018-02-01 15:28:45 +0100

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
+import { FCM } from '@ionic-native/fcm';
+
+
+declare var FCMPlugin: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,44 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, private platform: Platform, private alertCtrl: AlertController, private fcm: FCM) {
+    this.onNotification();
   }
 
+async onNotification(){
+  try {
+    await this.platform.ready();
+    
+    FCMPlugin.getToken(
+      (t) => {
+        console.log(t);
+      },
+      (e) => {
+        console.log(e);
+      }
+    );
+    
+    FCMPlugin.onNotification(
+      (data) => {
+        console.log(data);
+      },
+      (e) => {
+        console.log(e);
+      }
+    );
+
+    /*
+    FCMPlugin.onNotification((data) => {
+      this.alertCtrl.create({
+        message: data.message
+      }).present();
+    })
+    .error((error)=>{
+      console.log(error);
+    })*/
+
+  } catch (error) {
+    
+  }
+}
 }
